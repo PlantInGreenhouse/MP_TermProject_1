64f6bc1e6382793a6187484694f78ae9382b78c4
PlantInGreenhouse <seongth990319@gmail.com>
2024-06-07 20:34:32 +0900

    거의 다 돼가

--- app/src/main/java/org/androidtown/termproject/lobby_3.java
+++ app/src/main/java/org/androidtown/termproject/lobby_3.java
@@ -2,24 +2,41 @@ package org.androidtown.termproject;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-import org.androidtown.termproject.R;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
+
 public class lobby_3 extends AppCompatActivity {
+
+    private LinearLayout newClassContainer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lobby_3);
+
         ImageButton button1 = findViewById(R.id.cartIcon);
         ImageButton button3 = findViewById(R.id.marketIcon);
         ImageButton button2 = findViewById(R.id.studyIcon);
         ImageButton button4 = findViewById(R.id.myPageIcon);
-
+        newClassContainer = findViewById(R.id.newClassContainer);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,5 +65,56 @@ public class lobby_3 extends AppCompatActivity {
             }
         });
 
+        loadNewClasses();
+    }
+
+    private void loadNewClasses() {
+        DatabaseReference lecturesRef = FirebaseDatabase.getInstance().getReference("users");
+
+        lecturesRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                newClassContainer.removeAllViews();
+
+                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                    for (DataSnapshot lectureSnapshot : userSnapshot.child("lectures").getChildren()) {
+                        Map<String, Object> lectureData = (Map<String, Object>) lectureSnapshot.getValue();
+
+                        if (lectureData != null) {
+                            String title = (String) lectureData.get("title");
+                            String description = (String) lectureData.get("contents");
+                            String thumbnailUrl = (String) lectureData.get("thumbnail");
+
+                            addNewClassView(title, description, thumbnailUrl);
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(lobby_3.this, "Failed to load data.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @SuppressLint("InflateParams")
+    private void addNewClassView(String title, String description, String thumbnailUrl) {
+        View classView = getLayoutInflater().inflate(R.layout.activity_lobby, null);
+
+        ImageView thumbnail = classView.findViewById(R.id.thumbnail);
+        TextView titleView = classView.findViewById(R.id.title);
+        TextView descriptionView = classView.findViewById(R.id.description);
+
+        titleView.setText(title);
+        descriptionView.setText(description);
+
+        if (thumbnailUrl != null && !thumbnailUrl.isEmpty()) {
+            Glide.with(this).load(thumbnailUrl).into(thumbnail);
+        } else {
+            thumbnail.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
+        }
+
+        newClassContainer.addView(classView);
     }
 }
